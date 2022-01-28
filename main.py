2df1d1fa5f06243b2222a60ce166b9190a12c752
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2022-01-28 13:39:47 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import math
+from tkinter import *
+
+root = Tk()
+
+
+CanvasSize= 1000
+
+canvas = Canvas(root,height=CanvasSize,width=CanvasSize)
+canvas.pack()
+
+def CreateCircle(x,y,r,canvasName,ColorCode):
+    x0 = x - r
+    y0 = y - r
+    x1 = x + r
+    y1 = y + r
+    return canvasName.create_oval(x0,y0,x1,y1,outline=ColorCode)
+
+def PlotPixel(x, y, canvas,ColorCode):
+    return CreateCircle(x,y,1,canvas,ColorCode)
+
+def CreateWallpaper():
+    corna = 5
+    cornb = 5
+    side = 20
+
+    for i in range(0,CanvasSize):
+        for j in range(0,CanvasSize):
+            x = corna + i * side / 100
+            y = cornb + j * side / 100
+            c = math.floor(x * x + y * y)
+            if(c % 2 == 0):
+                PlotPixel(i,j,canvas, "red")
+            elif(c % 3 == 0):
+                PlotPixel(i,j,canvas, "yellow")
+            else:
+                PlotPixel(i,j,canvas, "blue")
+
+
+
+
+
+CreateWallpaper()
+root.mainloop()
